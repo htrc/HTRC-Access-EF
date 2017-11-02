7a916ab051081cad131a3d6607d0556317afabbc
David Bainbridge <dbbridge@lis-host.lis.illinois.edu>
2017-11-02 02:03:38 -0500

    Some basic tokenizeation

--- src/main/java/org/hathitrust/extractedfeatures/action/ICUTokenizeAction.java
+++ src/main/java/org/hathitrust/extractedfeatures/action/ICUTokenizeAction.java
@@ -2,6 +2,9 @@ package org.hathitrust.extractedfeatures.action;
 
 
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -43,10 +46,43 @@ public class ICUTokenizeAction extends BaseAction
 	public void doAction(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException
 	{
+	    boolean lowercase_filter = false;
+	    ArrayList<String> words = new ArrayList<String>();
+	    
 		String text_in = request.getParameter("text-in");
 		
 		if (text_in != null) {
-
+		    Reader reader = new StringReader(text_in);
+		    
+		    ICUTokenizer icu_tokenizer = new ICUTokenizer();
+		    icu_tokenizer.setReader(reader);
+		    
+		    CharTermAttribute charTermAttribute = icu_tokenizer.addAttribute(CharTermAttribute.class);
+		    
+		    TokenStream token_stream = null;
+		    
+		    if (lowercase_filter) {
+			token_stream = new LowerCaseFilter(icu_tokenizer);
+		    }
+		    else {
+			token_stream = icu_tokenizer;
+		    }
+		    
+		    try {
+			token_stream.reset();
+			
+			while (token_stream.incrementToken()) {
+			    String term = charTermAttribute.toString();
+			    words.add(term);
+			}
+			
+			token_stream.end();
+			token_stream.close();
+		    }
+		    catch (IOException e) {
+			e.printStackTrace();
+		    }
+		    
 		}
 		else {
 		    response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing 'text-in' parameter to " + getHandle());
