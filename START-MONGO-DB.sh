06f25e56cb0dcc685af22e7a075ecf82295840e5
davidbwaikato <davidb@waikato.ac.nz>
2018-02-23 15:39:13 +1300

    Template script to help get mongodb going

---
+++ START-MONGO-DB.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo ""
+echo "******"
+echo "Some different ways to start MongoDB:"
+echo ""
+
+echo "  sudo system mongodb start"
+echo "  /usr/bin/mongod --config /etc/mongodb.conf"
+echo "  /usr/local/mongodb/bin/mongod --dbpath /usr/local/mongodb/db/"
+
+echo ""
+echo "Edit this script to choose one of these, or some alternative that is meaningful for your OS"
+echo "  ... then comment out this echo statement!"
+echo "******"
+
+# sudo system mongodb start
+# /usr/bin/mongod --config /etc/mongodb.conf
+# /usr/local/mongodb/bin/mongod --dbpath /usr/local/mongodb/db/
