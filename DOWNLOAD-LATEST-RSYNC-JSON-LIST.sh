fc9e342ee8e9bc716c66a0d52e37451591bcdefc
David Bainbridge <dbbridge@lis-host.lis.illinois.edu>
2018-04-19 17:48:11 -0500

    More direct way to get the json list and put it in the right place for the web server

--- DOWNLOAD-LATEST-RSYNC-JSON-LIST.sh
+++ DOWNLOAD-LATEST-RSYNC-JSON-LIST.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-rsync -azv --progress data.analytics.hathitrust.org::features/listing/htrc-ef-all-files.txt .
+echo "Ensuring list of IDs is up to date with rsync server"
+rsync -azv --progress data.analytics.hathitrust.org::features/listing/htrc-ef-all-files.txt src/main/resources/.
 
-echo
-echo "Copying (-i) 'htrc-ef-all-files.txt' to:"
-echo "  'src/main/resoures'"
-echo
-echo "/bin/cp -i htrc-ef-all-files.txt ./src/main/resources/htrc-ef-all-files.txt"
-
-/bin/cp -i htrc-ef-all-files.txt ./src/main/resources/htrc-ef-all-files.txt
+#echo
+#echo "Copying (-i) 'htrc-ef-all-files.txt' to:"
+#echo "  'src/main/resoures'"
+#echo
+#echo "/bin/cp -i htrc-ef-all-files.txt ./src/main/resources/htrc-ef-all-files.txt"
+#
+#/bin/cp -i htrc-ef-all-files.txt ./src/main/resources/htrc-ef-all-files.txt
