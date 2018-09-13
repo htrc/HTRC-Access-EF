3111f250dd02b50bb7b3a08766c688551e813bf3
David Bainbridge <davidb@waikato.ac.nz>
2018-09-13 05:57:00 -0500

    Added 'x' permission; added echo statement

--- SETUP-LCC-PDF-JSON-TREE.sh
+++ SETUP-LCC-PDF-JSON-TREE.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Extracting lcc-pdf-to-json-master/results.json from zip file"
+echo "  => src/main/resources/lcc-outline-treemap.json"
+
 unzip -c lcc-pdf-to-json-master.zip lcc-pdf-to-json-master/results.json  \
       >  src/main/resources/lcc-outline-treemap.json
