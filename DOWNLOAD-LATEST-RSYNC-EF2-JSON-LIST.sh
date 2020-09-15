ad3e86ac60973d5d6246a1859ac1eca2095c9b49
David Bainbridge <davidb@waikato.ac.nz>
2020-09-15 05:29:31 -0500

    Tidy up

--- DOWNLOAD-LATEST-RSYNC-EF2-JSON-LIST.sh
+++ DOWNLOAD-LATEST-RSYNC-EF2-JSON-LIST.sh
@@ -4,12 +4,13 @@ echo "****"
 echo "* Ensuring file-list of IDs is up to date with rsync server"
 echo "****"
 
-output_stubby_file="htrc-ef2-all-files.txt"
+output_stubby_file="htrc-ef20-all-files.txt"
+full_output_stubby_file="src/main/resources/$output_stubby_file"
 
-rsync -azv queenpalm.ischool.illinois.edu::features-2020.03/listing/file_listing.txt "src/main/resources/$output_stubby_file"
+rsync -azv queenpalm.ischool.illinois.edu::features-2020.03/listing/file_listing.txt "$full_output_stubby_file"
 
 echo "****"
-echo "* Saved as '$output_stubby_file'"
+echo "* Saved as '$full_output_stubby_file'"
 echo "****"
 
 
